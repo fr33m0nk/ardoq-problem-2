ba66112c12e8de69148d123e2fc25c0c52cf34d6
Prashant Sinha <2052-prashant.sinha@users.noreply.source.golabs.io>
2021-06-26 07:20:08 +0530

    [fr33m0nk]: removes ununsed imports

--- src/fr33m0nk/ardoq/core.clj
+++ src/fr33m0nk/ardoq/core.clj
@@ -1,11 +1,9 @@
 (ns fr33m0nk.ardoq.core
   (:require
-    [mount.lite :as mount]
-    [fr33m0nk.ardoq.server :as server])
+    [mount.lite :as mount])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& _]
   (mount/start)
   (.addShutdownHook
